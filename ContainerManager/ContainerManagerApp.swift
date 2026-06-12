2c22c4d88d8c35effbf617b42f49cd6c120095ac
Bart Reardon <bart.reardon@csiro.au>
2026-06-12 22:35:52 +1000

    Initial Commit

---
+++ ContainerManager/ContainerManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContainerManagerApp.swift
+//  ContainerManager
+//
+//  Created by Bart E Reardon on 12/6/2026.
+//
+
+import SwiftUI
+
+@main
+struct ContainerManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
